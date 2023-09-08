1c2dd15a9f4d320dbaf3acc5a9a413ce81bc7913
Theodros Gezu Yeshalem <teddyegezu@gmail.com>
2023-09-08 02:00:31 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0 if successful
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
