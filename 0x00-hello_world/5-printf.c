a16c578b8f6e6c4b6a893b0da153526a4ddcfe15
Theodros Gezu Yeshalem <teddyegezu@gmail.com>
2023-09-08 03:57:51 -0700

    still unable to understand betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/***
-* main
-*/
+
+/**
+ * main - prints a string
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
