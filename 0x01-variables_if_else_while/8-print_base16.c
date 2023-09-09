a3a28d9d0d4ffa09730e1e1840769d61a77006ec
Theodros Gezu Yeshalem <teddyegezu@gmail.com>
2023-09-09 01:13:06 -0700

    All upto advanced

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+			putchar(num + '0');
+		else
+			putchar(num - 10 + 'a');
+	}
+	putchar('\n');
+
+	return (0);
+}
